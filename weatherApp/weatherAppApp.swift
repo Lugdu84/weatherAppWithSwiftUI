9fb8dd53df9cff13f3e65adeb943a3c74cfaa401
David Grammatico <david.lugdunum@gmail.com>
2023-07-17 10:35:10 +0100

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by David Grammatico on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
